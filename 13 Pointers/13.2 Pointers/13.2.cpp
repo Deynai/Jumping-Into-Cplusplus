54a1223506043d7ca440037854c5c0514fc0303b
Rowan <rowan.p@hotmail.co.uk>
2018-05-15 22:52:41 +0100

    13.1 and 13.2 added

---
+++ 13 Pointers/13.2 Pointers/13.2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+	int exitprogram;
+	std::cout << "This is the program for question 2 from chapter 13.\n";
+
+	std::cout << "Exiting.";
+	std::cin >> exitprogram;
+	return 0;
+}
