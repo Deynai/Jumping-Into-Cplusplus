ac8709a9708d9a9db26bf1d4e20f7c6fe2d7c553
Rowan <rowan.p@hotmail.co.uk>
2018-05-16 06:06:48 +0100

    add 13.3

--- 13 Pointers/13.2 Pointers/13.3.cpp
+++ 13 Pointers/13.2 Pointers/13.3.cpp
@@ -2,17 +2,19 @@
 #include <string>
 
 /* 
-1. Write a function that prompts the user to enter his or her first name and last name, as two separate
-values. This function should return both values to the caller via additional pointer(or reference)
-parameters that are passed to the function. 
+3. Modify the program you wrote for exercise 1 so that instead of always prompting the user for a last
+name, it does so only if the caller passes in a NULL pointer for the last name. 
 */
 
 void get_name(std::string* first, std::string* last)
 {
 	std::cout << "Enter first name.\n";
 	std::cin >> *first;
-	std::cout << "Enter last name.\n";
-	std::cin >> *last;
+	if (last == NULL)
+	{
+		std::cout << "Enter last name.\n";
+		std::cin >> *last;
+	}
 	return;
 }
 
@@ -23,7 +25,11 @@ int main()
 	std::string *p_x = &x;
 	std::string *p_y = &y;
 
-	get_name(p_x,p_y);
+	get_name(p_x, p_y);
+
+	std::cout << *p_x << " " << *p_y << "\n"; // print first and last names
+
+	get_name(p_x, p_y);
 
 	std::cout << *p_x << " " << *p_y << "\n"; // print first and last names
 
