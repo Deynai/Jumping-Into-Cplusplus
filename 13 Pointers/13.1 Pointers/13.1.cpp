024ff0da604d2857b9d5d51fd8c21f179677fca5
Rowan <rowan.p@hotmail.co.uk>
2018-05-16 05:57:00 +0100

    13.1

--- 13 Pointers/13.1 Pointers/13.1.cpp
+++ 13 Pointers/13.1 Pointers/13.1.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
+#include <string>
+
+/* Write a function that prompts the user to enter his or her first name and last name, as two separate
+values. This function should return both values to the caller via additional pointer(or reference)
+parameters that are passed to the function. */
+
+void get_name(std::string* first, std::string* last)
+{
+	std::cout << "Enter first name.\n";
+	std::cin >> *first;
+	std::cout << "Enter last name.\n";
+	std::cin >> *last;
+	return;
+}
 
 int main()
 {
-	int exitprogram;
-	std::cout << "This is the program for question 1 from chapter 13.";
+	std::string x;
+	std::string y;
+	std::string *p_x = &x;
+	std::string *p_y = &y;
 
+	get_name(p_x,p_y);
+
+	std::cout << *p_x << " " << *p_y << "\n"; // print first and last names
+
+	int exitprogram;
 	std::cin >> exitprogram;
 	return 0;
 }
