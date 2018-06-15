3ed54ba3fd031ce2ed42f65b17843389b5e3acd6
Rowan <rowan.p@hotmail.co.uk>
2018-06-15 05:22:11 +0100

    16.5
    16.5 Hanoi finished and annotated

--- 16 Recursion/16.5 Towers of Hanoi/16.5.cpp
+++ 16 Recursion/16.5 Towers of Hanoi/16.5.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+// debug
+int move_counter = 0;
+
 struct disk
 {
 	int size;
@@ -19,42 +22,95 @@ disk* initStick(disk* stick, int n)
 	return stick;
 }
 
-// display current state of sticks, debug
+// display current state of sticks - used for debug
 void displaySticks(disk** stick)
 {
 	disk* currentDisk;
 	for (size_t i = 0; i < 3; i++)
 	{
+		std::cout << "\n";
 		currentDisk = stick[i];
-		std::cout << "Stick " << i+1 << ":\n";
+		std::cout << "Stick " << i+1 << ": ";
 		while (currentDisk != NULL)
 		{
-			std::cout << currentDisk->size << "\n";
+			std::cout << currentDisk->size << " ";
 			currentDisk = currentDisk->p_next;
 		}
 	}
 	return;
 }
 
-disk* moveDisk(disk** stick, int source, int target)
+void moveDisk(disk** stick, int source, int target)
 {
-	
+	// debug
+	move_counter += 1;
+
+	// debug - does nothing unless errors
+	// displaySticks(stick);
+	if (stick[source] == NULL)
+	{
+		std::cout << "\n\nILLEGAL MOVE - Tried to move a non-existent disk from an empty stick!";
+		return;
+	}
+	//---
+
+	disk* newDisk = new disk;
+	newDisk->size = stick[source]->size;
+	newDisk->p_next = stick[target];
+	stick[target] = newDisk;
+
+	disk* tempDisk = stick[source];
+	stick[source] = stick[source]->p_next;
+	delete tempDisk;
+
+	// debug - does nothing unless errors
+	if (newDisk->p_next != NULL)
+	{
+		if (newDisk->size > newDisk->p_next->size)
+		{
+			std::cout << "\n\nILLEGAL MOVE - Tried to place a big disk onto a smaller one!";
+		}
+	}
+	//---
+
+	return;
+}
+
+// meat and potatoes, this solves n hanoi disks moving from stick[source] to stick[target], lots of recursion inside
+void solveHanoi(disk** stick, int n, int source, int target)
+{
+	if (n == 1) // base case of only moving the smallest disk
+	{
+		moveDisk(stick, source, target);
+	}
+	else
+	{
+		solveHanoi(stick, n - 1, source, 3 - source - target); // solve n-1 hanoi disks from the source to the free stick
+		moveDisk(stick, source, target); // move the biggest nth disk from the source to the target stick
+		solveHanoi(stick, n - 1, 3 - source - target, target);  // solve the n-1 hanoi disks from the free stick to the target stick
+	}
+	return;
 }
 
 int main()
 {
-	disk** stick = new disk*[3];
+	disk** stick = new disk*[3]; // pointer to array, stick[i] is a pointer to the first element of a linked list storing the disks on stick i+1
 	for (size_t i = 0; i < 3; i++)
 	{
-		stick[i] = NULL;
+		stick[i] = NULL; // empty sticks
 	}
-	int n = 5; // number of disks
-	stick[0] = initStick(stick[0], n);
+	int n = 20; // number of disks
+	stick[0] = initStick(stick[0], n); // initialises disks on stick 1
 
+	solveHanoi(stick, n, 0, 2); // start of recursion, no need to return anything as stick is a **pp and changes to each stick[i] will persist
+
+	// debug
 	displaySticks(stick);
+	std::cout << "\n\nNumber of Moves: " << move_counter;
+	// -- 
 
 	int exitprogram;
-	std::cout << "\nExiting.";
+	std::cout << "\n\nExiting.";
 	std::cin >> exitprogram;
 	return 0;
 }
